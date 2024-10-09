ff4313ea423e3f28fecea665e457f71339b9d69a
Neacsu Flavius Catalin <kete102@gmail.com>
2024-10-09 14:27:53 +0200

    chore(package.json): package.json
    package.json

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-namespace */
@@ -13,7 +12,9 @@ const boards_1 = require("./routes/boards");
 const auth_1 = require("./routes/auth");
 const tasks_1 = require("./routes/tasks");
 const db_1 = require("./db/db");
-const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
+const HOST = '0.0.0.0';
+const port = process.env.PORT || '3000';
+const PORT = parseInt(port, 10);
 const app = (0, express_1.default)();
 (0, db_1.connectToDB)();
 app.use(express_1.default.json());
@@ -22,10 +23,20 @@ app.use((0, cors_1.default)({
     methods: ['POST', 'GET', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+app.get('/', (_req, res) => {
+    res.status(200).json({
+        message: 'Hola',
+    });
+});
+app.get('/ping', (_req, res) => {
+    res.json({
+        message: 'pong',
+    });
+});
 app.use('/api/auth', auth_1.authRoutes);
 app.use('/api/boards', boards_1.boardRoutes);
 app.use('/api/tasks', tasks_1.tasksRoutes);
-app.listen(port, () => {
+app.listen(PORT, HOST, () => {
     console.log(`Server lintening on port: ${port}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.get('/', (_req, res) => {
 })
 
 app.get('/ping', (_req, res) => {
-	return res.json({
+	res.json({
 		message: 'pong',
 	})
 })
