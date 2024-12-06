bc8195da73e3b0b85685f3f283691df71ec64f13
Neacsu Flavius Catalin <kete102@gmail.com>
2024-12-06 19:35:01 +0100

    fix(webhook secret config): webhook secret config
    webhook secret config

--- src/config.ts
+++ src/config.ts
@@ -5,14 +5,16 @@ const ClerkClient = createClerkClient({
 })
 const port = process.env.PORT || '3000'
 export let MONGODB_CONNECTION_STRING = ''
+export let CLERK_WEBHOOK_SECRET_KEY = ''
 
 if (process.env.NODE_ENV === 'development') {
 	MONGODB_CONNECTION_STRING = process.env
 		.LOCAL_MONGODB_CONNECTION_STRING as string
+	CLERK_WEBHOOK_SECRET_KEY = process.env.LOCAL_WEBHOOK_SECRET as string
 } else if (process.env.NODE_ENV === 'production') {
 	MONGODB_CONNECTION_STRING = process.env.MONGO_URL as string
+	CLERK_WEBHOOK_SECRET_KEY = process.env.WEBHOOK_SECRET as string
 }
 
-export const CLERK_WEBHOOK_SECRET_KEY = process.env.WEBHOOK_SECRET as string
 export const PORT = parseInt(port, 10)
 export default ClerkClient
