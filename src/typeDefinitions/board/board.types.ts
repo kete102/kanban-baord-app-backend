9b94fa995f8613a0a7b84bc69799a86f7f4eb76d
Neacsu Flavius Catalin <kete102@gmail.com>
2024-12-06 18:32:40 +0100

    feat(types): board and tasks types
    board and tasks types

--- src/typeDefinitions/board/board.types.ts
+++ src/typeDefinitions/board/board.types.ts
@@ -3,5 +3,6 @@ import {IBoard} from 'src/models/BoardModel'
 export interface BoardsService {
 	success: boolean
 	message?: string
+	error?: string
 	board?: IBoard[] | IBoard
 }
---
+++ src/typeDefinitions/task/task.types.ts
@@ -0,0 +1,34 @@
+import {
+	createTaskZodSchema,
+	deleteTaskZodSchema,
+} from 'src/helpers/validateTaskRequestBody'
+import {ITask} from 'src/models/TaskModel'
+
+export interface TasksService {
+	success: boolean
+	message?: string
+	error?: string
+	tasks?: ITask[] | ITask | null
+}
+
+export interface GetAllTasks {
+	clerkId: string
+	boardId: string
+}
+
+export interface CreateTask {
+	clerkId: string
+	boardId: string
+	taskData: Zod.infer<typeof createTaskZodSchema>
+}
+
+export interface UpdateStatus {
+	userId: string
+	taskId: string
+	newStatus: string
+}
+
+export interface DeleteTask {
+	clerkId: string
+	taskData: Zod.infer<typeof deleteTaskZodSchema>
+}
