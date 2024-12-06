90c048d4d182b339a0d2fa76a47128944231b8f3
Neacsu Flavius Catalin <kete102@gmail.com>
2024-12-06 18:32:01 +0100

    fix(db): db connection config
    db connection config

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,16 +1,22 @@
-import mongoose from 'mongoose'
+import mongoose, {MongooseError} from 'mongoose'
 import 'dotenv/config'
-
-const MONGODB_CONNECTION_STRING = process.env
-	.MONGODB_CONNECTION_STRING as string
+import {MONGODB_CONNECTION_STRING} from 'src/config'
 
 export function connectToDB() {
-	mongoose
-		.connect(MONGODB_CONNECTION_STRING)
-		.then(() => {
-			console.log('DB connected')
-		})
-		.catch((error) => {
-			console.error('Error connecting to DB', error)
-		})
+	console.log(MONGODB_CONNECTION_STRING)
+	try {
+		mongoose
+			.connect(MONGODB_CONNECTION_STRING)
+			.then(async () => {
+				console.log(`Database Name: ${mongoose.connection.db?.databaseName}`)
+			})
+			.catch((error) => {
+				console.error('Error connecting to DB', error)
+			})
+	} catch (error) {
+		if (error instanceof MongooseError) {
+			console.log(`Error connecting to DB: ${error.message}`)
+		}
+		console.log(`DB Error: ${error}`)
+	}
 }
