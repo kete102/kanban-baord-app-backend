8b6382677d45fc67aea65638970f14c8f7c58d38
Neacsu Flavius Catalin <kete102@gmail.com>
2024-12-06 18:31:13 +0100

    fix(config): variable enviroments
    config

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,17 @@ import {createClerkClient} from '@clerk/clerk-sdk-node'
 const ClerkClient = createClerkClient({
 	secretKey: process.env.CLERK_SECRET_KEY,
 })
+const port = process.env.PORT || '3000'
+export let MONGODB_CONNECTION_STRING = ''
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+if (process.env.NODE_ENV === 'development') {
+	MONGODB_CONNECTION_STRING = process.env
+		.LOCAL_MONGODB_CONNECTION_STRING as string
+} else if (process.env.NODE_ENV === 'production') {
+	MONGODB_CONNECTION_STRING = process.env.MONGO_URL as string
+}
 
 export const CLERK_WEBHOOK_SECRET_KEY = process.env.WEBHOOK_SECRET as string
-
-const port = process.env.PORT || '3000'
 export const PORT = parseInt(port, 10)
 export default ClerkClient
